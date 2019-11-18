71e06cdf19110d6b4f81296eb679c58fc08f3299
HamletPrc <3241753461@qq.com>
2019-11-18 13:16:27 +0800

    little change

--- gasTable/source/gasTable.C
+++ gasTable/source/gasTable.C
@@ -37,14 +37,15 @@ using namespace Garfield;
 using namespace std;
 
 void DefineGas(const char *gas1, double rat1, const char *gas2, double rat2, const char *filename);
+void DefineGas(const char *gas1, double rat1, const char *gas2, double rat2, const char *gas3, double rat3, const char *filename);
 
 int main(){
-    int proportion;
-    for(proportion=10;proportion>0;proportion--){
-        char filename[20];
-		sprintf( filename, "%s%d%s", "Ar_iC4H10_", proportion,".gas");
-        DefineGas("argon", 100-proportion, "iC4H10", proportion, filename);
-    }
+    // for(proportion=10;proportion>0;proportion--){
+    //     char filename[20];
+	// 	sprintf( filename, "%s%d%s", "Ar_iC4H10_", proportion,".gas");
+    //     DefineGas("argon", 100-proportion, "iC4H10", proportion, filename);
+    // }
+    DefineGas("argon", 90, "CH4", 3, "CO2", 7, "Ar_90_CH4_3_CO2_7.gas");
         
     return 0;
 }
@@ -57,10 +58,7 @@ void DefineGas(const char *gas1, double rat1, const char *gas2, double rat2, con
     MediumMagboltz *gas;
     gas = new MediumMagboltz();
 
-    cout<<"?"<<endl;
-
-    if(!gas->LoadGasFile(filename)) 
-    {
+    if(!gas->LoadGasFile(filename)){
         gas->SetComposition(gas1, rat1, gas2, rat2);
         //gas->SetComposition(gas1, rat1);
         gas->SetTemperature(293.15);
@@ -81,7 +79,53 @@ void DefineGas(const char *gas1, double rat1, const char *gas2, double rat2, con
         gas->EnablePenningTransfer(rPenning, lambdaPenning, "ar");
         // Generate the gas table file for electron drift
         gas->GenerateGasTable(1,false);
-	    gas->WriteGasFile(filename);
+    }
+
+    double ex;
+	double vx, vy, vz;
+	double dl, dt;
+	double alpha; 
+	double eta;
+
+    for(int i=1; i<100; i++)
+	{
+	    ex = i*10;
+	    gas->ElectronVelocity  (ex, 0, 0, 0, 0, 0, vx, vy, vz);
+            gas->ElectronDiffusion (ex, 0, 0, 0, 0, 0, dl, dt);
+            gas->ElectronTownsend  (ex, 0, 0, 0, 0, 0, alpha);
+            gas->ElectronAttachment(ex, 0, 0, 0, 0, 0, eta);
+
+	    cout<<"E = "<<ex<<"V/cm,   Vx ="<<vx*100<<"cm/us; alpha = "<<alpha<<"/cm"<<endl;
+    }
+}
+
+
+void DefineGas(const char *gas1, double rat1, const char *gas2, double rat2, const char *gas3, double rat3, const char *filename)
+{
+    MediumMagboltz *gas;
+    gas = new MediumMagboltz();
+
+    if(!gas->LoadGasFile(filename)){
+        gas->SetComposition(gas1, rat1, gas2, rat2, gas3, rat3);
+        //gas->SetComposition(gas1, rat1);
+        gas->SetTemperature(293.15);
+        gas->SetPressure(760);  //In Huang XueFeng's work, he uses 0.8atm=0.8*760mmHg gas-pressure
+        gas->EnableDebugging();
+        gas->Initialise();
+        gas->DisableDebugging();
+        
+        gas->SetMaxElectronEnergy(200.); //eV
+        gas->SetMaxPhotonEnergy(200.);
+        gas->EnableEnergyRangeAdjustment(true);
+        gas->EnableAnisotropicScattering();
+        
+        gas->Initialise(true);
+        // Set the Penning transfer efficiency.
+        const double rPenning = 0.57;
+        const double lambdaPenning = 0.;
+        gas->EnablePenningTransfer(rPenning, lambdaPenning, "ar");
+        // Generate the gas table file for electron drift
+        gas->GenerateGasTable(1,false);
     }
 
     double ex;
