cd0e33ee4e79998d001ac47cdbbe169f8a2be1c1
HamletPrc <3241753461@qq.com>
2019-11-19 15:57:16 +0800

    Add plot.py for Gas Component

---
+++ plot.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+filename = 'temp.dat'
+X,Y = [],[]
+with open(filename, 'r') as f:#1
+    lines = f.readlines()#2
+    for line in lines:#3
+        value = [float(s) for s in line.split()]#4
+        X.append(value[0])#5
+        Y.append(value[1])
+
+print(X)
+print(Y)
+
+plt.plot(X, Y, marker='o')
+plt.xlabel('Field Strength/(V/cm)')
+plt.ylabel('Electron Drift Velocity/(cm/us)')
+plt.title('Electron Drift Velocity for T2K')
+plt.grid()
+plt.legend()
+plt.show()
+
